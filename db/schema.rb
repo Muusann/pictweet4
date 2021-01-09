65b692a252c8f291167540450fa88b9514e0780a
Muusann <73873796+Muusann@users.noreply.github.com>
2021-01-09 19:45:21 +0900

    表示の並び替え

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_07_081228) do
+ActiveRecord::Schema.define(version: 2021_01_09_101041) do
 
   create_table "tweets", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
